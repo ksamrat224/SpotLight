5515bcdcf21ff8c8aefc6ed0721220d777fa7446
ksamrat224 <ksamrat224@gmail.com>
2025-07-13 20:19:40 +0545

    clerk for authentication initialized

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,24 @@
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+if (!publishableKey) {
+  throw new Error("EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY is not set");
+}
+
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1,backgroundColor:"black" }}>
-       <Stack screenOptions={{headerShown:false}} />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <ClerkLoaded>
+
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+          <Stack screenOptions={{ headerShown: false }} />
+        </SafeAreaView>
+      </SafeAreaProvider>
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
