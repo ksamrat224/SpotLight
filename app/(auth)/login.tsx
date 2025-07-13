50a5e0e495f4c296e0819b0ab250977cbda5d3ed
ksamrat224 <ksamrat224@gmail.com>
2025-07-13 20:57:16 +0545

    google authenticatio done

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,10 +1,25 @@
 import { COLORS } from "@/constants/theme";
 import { styles } from "@/styles/auth.styles";
+import { useSSO } from "@clerk/clerk-expo";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import { useRouter } from "expo-router";
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 const login = () => {
+    const {startSSOFlow}=useSSO();
+    const router = useRouter();
+    const handleGoogleLogin = async () => {
+        try{
+        const {createdSessionId,setActive}= await startSSOFlow({strategy:"oauth_google"});
+        if(setActive && createdSessionId){
+            setActive({session:createdSessionId});
+            router.replace("/(tabs)");
+        } }catch (error) {
+            console.error("Error during Google login:", error);
+        }
+    }
+
   return (
     <View style={styles.container}>
       {/*BRAND SECTION */}
@@ -24,6 +39,18 @@ const login = () => {
           style={styles.illustration}
           resizeMode="cover"
         />
+      </View>
+      <View style={styles.loginSection}>
+       <TouchableOpacity style={styles.googleButton} onPress={handleGoogleLogin}>
+        <View style={styles.googleIconContainer}>
+            <Ionicons name="logo-google" size={20} color={COLORS.surface} />
+        </View>
+        <Text style={styles.googleButtonText}>Continue with Google</Text>
+
+       </TouchableOpacity>
+       <Text style={styles.termsText}>
+        By continuing,you agree to out Terms and Privacy Policy
+       </Text>
       </View>
     </View>
   );
