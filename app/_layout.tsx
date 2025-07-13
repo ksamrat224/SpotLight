81d111cb5aea3230edd9ef04b57a8f6e5f311e7f
ksamrat224 <ksamrat224@gmail.com>
2025-07-13 18:49:05 +0545

    safeareaprovider and safeareaview

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,27 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return(
-   <Stack>
-    <Stack.Screen
-      name="index"
-      options={{
-        headerShown: false,
-      }}
-    />
-   </Stack>
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              headerShown: false,
+              title: "Home",
+            }}
+          />
+          <Stack.Screen
+            name="notifications"
+            options={{
+              title: "Notifications",
+              headerShown: true,
+            }}
+          />
+        </Stack>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
