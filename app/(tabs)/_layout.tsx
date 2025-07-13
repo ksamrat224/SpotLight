399ff46e5f8756eee66d06696a5efa0003a3fe96
ksamrat224 <ksamrat224@gmail.com>
2025-07-13 19:15:32 +0545

    layout tabs made for all name

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,51 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
 import React from "react";
 
 const TabLayout = () => {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="bookmarks" />
-      <Tabs.Screen name="create" />
-      <Tabs.Screen name="notifications" />
-      <Tabs.Screen name="profile" />
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="bookmarks"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="bookmark" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="add-circle" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen name="notifications"
+      options={{
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="heart" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen name="profile" 
+      options={{
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="person-circle" size={size} color={color} />
+          ),
+        }}/>
     </Tabs>
   );
 };
