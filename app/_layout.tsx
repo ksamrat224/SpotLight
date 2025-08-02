1e793e93d0f0236d87aee7a421288062c5df7a9a
ksamrat224 <ksamrat224@gmail.com>
2025-08-02 20:22:32 +0545

    layout wrapped with ClerConvexProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,18 @@
 import InitialLayout from "@/components/initialLayout";
-import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
-import { tokenCache } from "@clerk/clerk-expo/token-cache";
-import { Stack } from "expo-router";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
-if (!publishableKey) {
-  throw new Error("EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY is not set");
-}
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <ClerkLoaded>
-
+    <ClerkAndConvexProvider>
       <SafeAreaProvider>
         <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
-          <InitialLayout/>
+          <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
-      </ClerkLoaded>
-    </ClerkProvider>
+    </ClerkAndConvexProvider>
   );
 }
+
+  
